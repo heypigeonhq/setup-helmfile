fcc819933eda324bd1e977330406f4193609ef9b
Anthony Smith <420061+anthonator@users.noreply.github.com>
2023-09-04 06:21:52 -0400

    chore: setup test harness

---
+++ __tests__/main.test.ts
@@ -0,0 +1,28 @@
+import path from "path";
+
+import { Act } from "@kie/act-js";
+import { MockGithub } from "@kie/mock-github";
+
+let github: MockGithub;
+
+beforeEach(async () => {
+  github = new MockGithub({
+    repo: {
+      setupHelmfile: {},
+    },
+  });
+
+  await github.setup();
+});
+
+afterEach(async () => {
+  await github.teardown();
+});
+
+test("it works", async () => {
+  const act = new Act(github.repo.getPath("setupHelmfile"));
+
+  const result = await act.runEvent("push", { logFile: "act.log" });
+
+  expect(result).toStrictEqual([]);
+});
